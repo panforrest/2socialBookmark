d8e54e56c822aa77554433a3a35f4306def3fe33
panforrest <guoqianp@gmail.com>
2017-03-16 17:37:04 -0400

    in Profiles.js use superagent to call api/profile

--- src/components/containers/Profile.js
+++ src/components/containers/Profile.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
+import superagent from 'superagent'
 
 class Profile extends Component {
+	componentDidMount(){
+        // console.log('componentDidMount: ')
+		superagent
+		.get('/api/profile')
+		.query(null)
+		.set('Accept', 'application/json')
+		.end((err, response) => {
+			if (err) {
+				const message = err.message || err
+				alert(message)
+				return
+			}
+            console.log(JSON.stringify(response.body))    
+		})
+
+
+	}
+
 	render(){
+
+
 		return(
 			<div>
 			    This is Profile container
-
+			    <list />
 			</div>
-
 		)
 	}
-
 }
 
 export default Profile
